2e0fc04079978337423a783213e33249b6ec0d78
ddavison <daniel.jj.davison@gmail.com>
2014-11-20 09:24:49 -0500

    refactored code

--- app.rb
+++ app.rb
@@ -18,81 +18,57 @@ get '/list' do
   list
 end
 
-get '/star.svg' do
-  if params[:user]
-    if params[:repo]
-      user     = params[:user]
-      repo     = params[:repo]
-      bg_color = params[:background] ? params[:background] : '4c1' # default bg color to green
-      color    = params[:color] ? params[:color] : 'fff' # default text color to white
+[:star, :fork].each do |type|
+  get "/#{type}.svg" do
+    if params[:user]
+      if params[:repo]
+        user     = params[:user]
+        repo     = params[:repo]
+        style    = params[:style] ? params[:style] : 'default'
+        bg_color = params[:background] ? params[:background] : '4c1' # default bg color to green
+        color    = params[:color] ? params[:color] : 'fff' # default text color to white
 
-      star_count = get("https://api.github.com/repos/#{user}/#{repo}", 'stargazers_count', {user: user, repo: repo})
+        case type
+          when :star
+            count = fetch("https://api.github.com/repos/#{user}/#{repo}", 'stargazers_count', {user: user, repo: repo})
+            count_url = "https://github.com/#{user}/#{repo}/stargazers"
+            button_url = "https://github.com/#{user}/#{repo}"
+          when :fork
+            count = fetch("https://api.github.com/repos/#{user}/#{repo}", 'forks_count', {user: user, repo: repo})
+            count_url = "https://github.com/#{user}/#{repo}/network"
+            button_url = "https://github.com/#{user}/#{repo}/fork"
+        end
 
-      # everything is ok.
-      content_type 'image/svg+xml'
+        # everything is ok.
+        content_type 'image/svg+xml'
 
-      # Avoid CDN caching
-      now = CGI::rfc1123_date(Time.now)
-      response.headers['Cache-Control'] = 'no-cache,no-store,must-revalidate,max-age=0'
-      response.headers["Date"] = now
-      response.headers["Expires"] = now
+        # Avoid CDN caching
+        now = CGI::rfc1123_date(Time.now)
+        response.headers['Cache-Control'] = 'no-cache,no-store,must-revalidate,max-age=0'
+        response.headers["Date"] = now
+        response.headers["Expires"] = now
 
-      return create_button({
-        :button_text => 'star',
-        :count_url   => "https://github.com/#{user}/#{repo}/stargazers",
-        :count       => star_count,
-        :button_url  => "https://github.com/#{user}/#{repo}",
-        :bg_color    => bg_color,
-        :color       => color
-      })
+        return create_button({
+           :button_text => type,
+           :count_url   => count_url,
+           :count       => count,
+           :button_url  => button_url,
+           :bg_color    => bg_color,
+           :color       => color,
+           :style       => style
+         })
+      else
+        content_type 'text/plain'
+        return 'missing repo param'
+      end
     else
       content_type 'text/plain'
-      return 'missing repo param'
+      return 'missing user param'
     end
-  else
-    content_type 'text/plain'
-    return 'missing user param'
   end
 end
 
-get '/fork.svg' do
-  if params[:user]
-    if params[:repo]
-      user     = params[:user]
-      repo     = params[:repo]
-      bg_color = params[:background] ? params[:background] : '4c1' # default bg color to green
-      color    = params[:color] ? params[:color] : 'fff' # default text color to white
-
-      fork_count = get("https://api.github.com/repos/#{user}/#{repo}", 'forks_count', {user: user, repo: repo})
-
-      # everything is ok.
-      content_type 'image/svg+xml'
-
-      # Avoid CDN caching
-      now = CGI::rfc1123_date(Time.now)
-      response.headers['Cache-Control'] = 'no-cache,no-store,must-revalidate,max-age=0'
-      response.headers["Date"] = now
-      response.headers["Expires"] = now
-
-      return create_button({
-        :button_text => 'fork',
-        :count_url   => "https://github.com/#{user}/#{repo}/network",
-        :count       => fork_count,
-        :button_url  => "https://github.com/#{user}/#{repo}/fork",
-        :bg_color    => bg_color,
-        :color       => color
-      })
-    else
-      content_type 'text/plain'
-      return 'missing repo param'
-    end
-  else
-    content_type 'text/plain'
-    return 'missing user param'
-  end
-end
-
-def get(api_url, prop, args)
+def fetch(api_url, prop, args)
   cached_response = "cache/#{args[:user]}:#{args[:repo]}"
   uri = URI.parse api_url
   http = Net::HTTP.new(uri.host, uri.port)
